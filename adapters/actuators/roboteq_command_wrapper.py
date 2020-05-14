741c34194b48347257222dcc0efe09a41897fe95
andycavatorta <andycavatorta@gmail.com>
2020-05-14 16:51:12 -0400

    debug

--- adapters/actuators/roboteq_command_wrapper.py
+++ adapters/actuators/roboteq_command_wrapper.py
@@ -148,7 +148,9 @@ class Controllers(threading.Thread):
 
         # are physical boards found for all boards defined in config?
         mcu_ids_from_boards = [board.read_internal_mcu_id() for board in self.boards.values()]
+        self.status_receiver("mcu_ids_from_boards",mcu_ids_from_boards)
         mcu_ids_in_config = self.config["boards"].keys()
+        self.status_receiver("mcu_ids_in_config",mcu_ids_in_config)
 
         if not (set(mcu_ids_in_config).issubset(set(mcu_ids_from_boards))):
             self.status_receiver("missing board", set(mcu_ids_in_config).difference(set(mcu_ids_from_boards)))
