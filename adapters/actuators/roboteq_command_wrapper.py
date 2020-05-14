2b15c02127c7d566f322cc8abc672b1b3040163a
andycavatorta <andycavatorta@gmail.com>
2020-05-14 17:19:13 -0400

    debug

--- adapters/actuators/roboteq_command_wrapper.py
+++ adapters/actuators/roboteq_command_wrapper.py
@@ -73,10 +73,10 @@ class Board(threading.Thread):
             time.sleep(0.05)
             self.serial.write(str.encode(serial_command +'\r'))
             echo_str = self._readSerial() # for serial echo
-            print("echo_str=", echo_str)
+            #print("echo_str=", echo_str)
             resp_str = self._readSerial()
-            print("resp_str=", resp_str)
-            print("callback",callback)
+            #print("resp_str=", resp_str)
+            #print("callback",callback)
             try:
                 callback(self.serial_device_path,resp_str)
             except TypeError as e: #if callback == None
