ad38ce32dea36c96c4442d7d74a1a9c96fde1463
andycavatorta <andycavatorta@gmail.com>
2020-05-14 16:19:39 -0400

    debug

--- reporting/exceptions/capture_exceptions.py
+++ reporting/exceptions/capture_exceptions.py
@@ -47,12 +47,14 @@ class Class:
         target_instance_dir = dir(target_instance_ref)
         for attribute in target_instance_dir:
             if isinstance(getattr(decorator_self.target_class, attribute, ""), BuiltinFunctionType):
-                print("---",attribute)
+                print("--->",attribute)
                 func_name = attribute
                 function_ref = getattr(target_instance_ref,attribute)
                 #apply_wrapper is a separate method to break the reference to wrapper() on each cycle of this loop
                 wrapper = decorator_self.apply_wrapper(function_ref,target_instance_ref, args, kwargs)
                 setattr(target_instance_ref, func_name, wrapper)
+            else:
+                print("####",attribute)
         return target_instance_ref
     def apply_wrapper(decorator_self, function_ref, target_instance_ref, *args, **kwargs):
         @functools.wraps(function_ref)
