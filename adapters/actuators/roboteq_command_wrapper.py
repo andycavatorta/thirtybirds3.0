13a4df50e272ee59f617d625992599316eef8a91
andycavatorta <andycavatorta@gmail.com>
2020-05-31 17:19:26 -0400

    testing

--- adapters/actuators/roboteq_command_wrapper.py
+++ adapters/actuators/roboteq_command_wrapper.py
@@ -1357,12 +1357,13 @@ class Controllers(threading.Thread):
         """
 
     def collect_boards(self, mcu_serial_device_path, resp_str):
+        # this method verifies that all mcu_ids listed in config can be matched with discovered boards.
         found = True
-        board_names_in_config = list(self.boards_config.keys())
-        board_names_found = list(self.boards.keys())
-        print(board_names_in_config, board_names_found)
-        #for board_name, board in self.boards.values():
-        #    print(board_name, board.read_internal_mcu_id())
+        mcu_ids_in_config = list(self.boards_config.keys())
+
+        for board in self.boards.values():
+            mcu_ids_in_config.remove(board.read_internal_mcu_id())
+            print(mcu_ids_in_config)
         #print(">>>", mcu_serial_device_path, resp_str)
         #print(self.boards.keys())
         #print(self.boards_config.keys())
