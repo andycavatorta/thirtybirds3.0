d943c7625f98d128dd9a4b10c24a724d328b427d
andycavatorta <andycavatorta@gmail.com>
2020-06-05 18:20:02 -0400

    ASYNC FLOW EXPERIMENTS

--- adapters/actuators/roboteq_command_wrapper.py
+++ adapters/actuators/roboteq_command_wrapper.py
@@ -394,7 +394,7 @@ class Board(threading.Thread):
             #print(resp_char)
             resp_str += resp_char.decode('utf-8')
         resp_str = resp_str[:-1] # trim /r from end
-        #print(self.serial_device_path, "resp_str",resp_str)
+        print("resp_str",resp_str)
         resp_l = resp_str.split('=')
         if len(resp_l) == 1:
             return resp_str
