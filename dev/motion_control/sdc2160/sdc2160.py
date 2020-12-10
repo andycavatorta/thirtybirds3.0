1dd28c6f5e6dfdbc1b0b8ba10a9340ee556f188d
Andy Cavatorta <hellyeah@media.mit.edu>
2020-12-10 17:22:03 -0500

    tests

--- dev/motion_control/sdc2160/sdc2160.py
+++ dev/motion_control/sdc2160/sdc2160.py
@@ -649,7 +649,8 @@ class Main(threading.Thread):
                 self, 
                 self.add_to_queue,
                 self.boards_config)
-            board.set_serial_echo(0)
+            board.set_serial_echo(1)
+            mcu_id = board.get_mcu_id(True)
             mcu_id = board.get_mcu_id(True)
             for name, val in self.boards_config.items():
                 if val["mcu_id"] == mcu_id:
