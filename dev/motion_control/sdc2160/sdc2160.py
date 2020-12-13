c5796a5095e9c1291c9ae123b0ab28400d2c38e2
Andy Cavatorta <hellyeah@media.mit.edu>
2020-12-13 16:44:24 -0500

    solving query update

--- dev/motion_control/sdc2160/sdc2160.py
+++ dev/motion_control/sdc2160/sdc2160.py
@@ -1913,8 +1913,8 @@ class Main(threading.Thread):
             ppr = motor_name,self.motors[motor_name].query(query_types.ENCODER_PPR)
             abs_position = motor_name,self.motors[motor_name].query(query_types.ENCODER_POSITION)
             print(">>>>",abs_position[1],ppr[1])
-            turns = abs_position[1] // ppr[1]
-            angle = abs_position[1] % ppr[1]
+            turns = abs(abs_position[1]) // abs(ppr[1]) * -1 if abs_position<0 else 1
+            angle = abs(abs_position[1]) % abs(ppr[1]) * -1 if abs_position<0 else 1
             print(abs_position[1], turns,angle)
 
     def add_to_queue(self, command, params={}):
