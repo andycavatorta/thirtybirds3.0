23a6667f1545377e95b2fd742141485d7040e22f
Andy Cavatorta <andycavatorta@gmail.com>
2021-03-03 23:28:03 +0000

    added single channel sequencer

--- adapters/actuators/chime_solenoid/test_74HC595_shift_reg.py
+++ adapters/actuators/chime_solenoid/test_74HC595_shift_reg.py
@@ -6,12 +6,30 @@ import HC595_shift_reg as shifter
 
 reg = shifter.HC595()
 
+seq = [ 1, 0, 0, 1, 1, 0, 0, 0 ]
+
+#seq = [ 1, 0, 0, 0, 1, 0, 0, 0,  1, 0, 1, 0, 1, 0, 1, 0,  1, 0, 0, 1, 0, 0, 1, 0,   1, 0, 0, 1, 0, 0, 1, 1 ]
+seq_step = 0
+
 val = [ 0 ]
 while True:
-    reg.write( val )
+
+    if seq[ seq_step ] == 1:    
+        val[ 0 ] = 0xff
+    else:
+        val[ 0 ] = 0x00
     print( val )
-    time.sleep( .1 )
-    val[ 0 ] = val[ 0 ] + 1
+    reg.write( val )
+    time.sleep( .010 )
+
+    val[ 0 ] = 0x00
+    print( val )
+    reg.write( val )
+    time.sleep( 0.165 )
+
+    seq_step = seq_step + 1
+    if seq_step >= 8:
+        seq_step = 0
     
     
 
