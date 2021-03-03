ec57394ba058b9fb349aeb620dccb04d8da2221c
Andy Cavatorta <andycavatorta@gmail.com>
2021-03-03 03:21:25 +0000

    cleaned up GPIO handling.  works more.

--- adapters/sensors/AMT203_absolute_encoder.py
+++ adapters/sensors/AMT203_absolute_encoder.py
@@ -2,8 +2,13 @@ import spidev
 import time
 import RPi.GPIO as GPIO
 
+
+def gpio_reset( pin_num ):
+    GPIO.setup( pin_num, GPIO.OUT)
+    GPIO.output( pin_num, GPIO.HIGH)
+
 class AMT203():
-  def __init__(self, bus=0, deviceId=0, cs=15, speed_hz=1000000):   # cs=16
+  def __init__(self, bus=0, deviceId=0, cs=5, speed_hz=1000000):   # cs=16
     self.deviceId = deviceId
     self.bus = bus
     self.cs = cs
@@ -15,10 +20,19 @@ class AMT203():
       self.spi.open(self.bus, self.deviceId)
       self.open = True
       self.spi.mode = 0b00
+      self.spi.no_cs = True
       print("SPI connected. Device id: ", self.deviceId)
     except:
       self.open = False
       print("Could not connect to SPI device")
+
+    # silly cleanup
+    gpio_reset(  8 )
+    gpio_reset(  7 )
+    gpio_reset( 18 )
+    gpio_reset( 17 )
+    gpio_reset( 16 )
+    gpio_reset(  5 )
 
     GPIO.setup(self.cs, GPIO.OUT)
     GPIO.output(self.cs, GPIO.HIGH)
@@ -28,7 +42,7 @@ class AMT203():
     while True:
       first_result = self.spiRW([0x00],self.speed,20)
       #first_result = self.spiRW([0x00],0,20)
-      #print( first_result )
+      print( first_result )
       if first_result[ 0 ] == 165:
         break;
     #first_result = self.spiRW([0x00],0,20)
@@ -36,10 +50,13 @@ class AMT203():
     #  first_result = self.spiRW([0x00],0,20)
 
   def get_position(self):
+    #GPIO.output(self.cs, GPIO.LOW)
     attempts = 0
     #print(">>> 1")
-    first_result = self.spiRW([0x10],self.speed,20)  # send rd_pos, expected nop_a5 return value
-    print( first_result[ 0 ], ', ', end='' )
+
+    # send rd_pos, expect nop_a5 return value
+    first_result = self.spiRW([0x10],self.speed,20)  
+    print( 'first res: ', first_result[ 0 ], ', ', end='' )
     #print(">>> 2")
 
     # keep sending NOP/0x00 as long as response is nop_a5
@@ -51,17 +68,23 @@ class AMT203():
         print(" yuk")
         return -1
       
-      if first_result != 0xa5:
+      if first_result[ 0 ] != 0x10:
+        print( 'got: ', first_result[0], ', ', end ='' )
         break;
-      #print( first_result[0] )
     #print(">>> 4")
 
+    attempts = 0
     while True:
       tmp = self.spiRW( [0x00], self.speed, 20 )
-      print( tmp, ', ', end='' )
+      print( tmp, ',, ', end='' )
       if tmp[ 0 ] == 0x10:
         msb_result = self.spiRW( [0x00], self.speed, 20 )
         break;
+      attempts = attempts + 1
+      if attempts > 100: 
+        #print(" yuk2 ")
+        #return -1
+        pass
       #if first_result[ 0 ] == 0x10:
 
     #msb_result = self.spiRW([0x00],self.speed,20)
@@ -77,6 +100,8 @@ class AMT203():
     #print(">>> 7")
     self.clean_buffer()
     #print(">>> 8")
+
+    #GPIO.output(self.cs, GPIO.HIGH )
     return final_result
 
   def set_zero(self):
@@ -93,6 +118,7 @@ class AMT203():
 
   def spiRW(self, values, speed, delay):
     GPIO.output(self.cs, GPIO.LOW)
+    time.sleep(.01)
     msg = self.spi.xfer(values, speed, delay)
     GPIO.output(self.cs, GPIO.HIGH)
     return msg
