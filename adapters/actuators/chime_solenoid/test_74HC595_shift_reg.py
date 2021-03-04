053b9d8dd61087776ba734e6919984b41a1ad238
Andy Cavatorta <andycavatorta@gmail.com>
2021-03-04 00:08:36 +0000

    made 5 track sequencer.  works!

--- adapters/actuators/chime_solenoid/test_74HC595_shift_reg.py
+++ adapters/actuators/chime_solenoid/test_74HC595_shift_reg.py
@@ -6,7 +6,19 @@ import HC595_shift_reg as shifter
 
 reg = shifter.HC595()
 
-seq = [ 1, 0, 0, 1, 1, 0, 0, 0 ]
+
+
+seq = [ [ 1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 1, 0 ],
+        [ 0, 0, 1, 0,  0, 0, 1, 0,  0, 0, 1, 0,  0, 0, 1, 0 ],
+        [ 0, 0, 0, 0,  1, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0 ],
+        [ 0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  1, 0, 0, 0 ],
+        [ 0, 1, 0, 1,  0, 1, 0, 1,  0, 1, 0, 1,  0, 0, 1, 1 ] ]
+        
+#seq = [ [ 1, 0, 0, 0,  0, 0, 0, 0  ],
+#        [ 0, 1, 0, 0,  0, 0, 0, 0  ],
+#        [ 0, 0, 1, 0,  0, 0, 0, 0  ],
+#        [ 0, 0, 0, 1,  0, 0, 0, 0  ],
+#        [ 0, 0, 0, 0,  1, 0, 0, 0  ] ]
 
 #seq = [ 1, 0, 0, 0, 1, 0, 0, 0,  1, 0, 1, 0, 1, 0, 1, 0,  1, 0, 0, 1, 0, 0, 1, 0,   1, 0, 0, 1, 0, 0, 1, 1 ]
 seq_step = 0
@@ -14,10 +26,14 @@ seq_step = 0
 val = [ 0 ]
 while True:
 
-    if seq[ seq_step ] == 1:    
-        val[ 0 ] = 0xff
-    else:
-        val[ 0 ] = 0x00
+    val[ 0 ] = 0;
+    
+    for trk in range( 0, 5 ):
+    
+        if seq[ trk ][ seq_step ] == 1:    
+            val[ 0 ] = val[ 0 ] + ( 1 << trk )
+            #val[ 0 ] = val[ 0 ] | 0xff
+            
     print( val )
     reg.write( val )
     time.sleep( .010 )
@@ -25,10 +41,10 @@ while True:
     val[ 0 ] = 0x00
     print( val )
     reg.write( val )
-    time.sleep( 0.165 )
+    time.sleep( 0.205 )
 
     seq_step = seq_step + 1
-    if seq_step >= 8:
+    if seq_step >= 16:
         seq_step = 0
     
     
