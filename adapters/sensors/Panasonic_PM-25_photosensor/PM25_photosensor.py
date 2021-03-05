8c54db01702fd2fc53dfb2283a46575a05d519ee
Andy Cavatorta <andycavatorta@gmail.com>
2021-03-05 19:46:47 +0000

    WORKS!  MVP

--- adapters/sensors/Panasonic_PM-25_photosensor/PM25_photosensor.py
+++ adapters/sensors/Panasonic_PM-25_photosensor/PM25_photosensor.py
@@ -1,5 +1,6 @@
-import spidev
+
 import time
+
 import RPi.GPIO as GPIO
 
 
@@ -7,25 +8,14 @@ def gpio_reset( pin_num ):
     GPIO.setup( pin_num, GPIO.OUT)
     GPIO.output( pin_num, GPIO.HIGH)
 
-class HC595():
-  def __init__(self, bus=0, deviceId=0, cs=25, speed_hz=1000000):  
-    self.deviceId = deviceId
-    self.bus = bus
-    self.cs = cs
-    self.speed = speed_hz
+class PM_25():
+  def __init__( self, gpio_pin = 8 ):  
+    self.gpio_pin = gpio_pin
     GPIO.setmode(GPIO.BCM)
     try:
-      print("bus: %s | pin: %s" % (self.bus, self.deviceId))
-      self.spi = spidev.SpiDev()
-      self.spi.open(self.bus, self.deviceId)
-      self.open = True
-      self.spi.mode = 0b00
-      self.spi.no_cs = True
-      self.spi.max_speed_hz = speed_hz
-      print("SPI connected. Device id: ", self.deviceId)
+      pass
     except:
-      self.open = False
-      print("Could not connect to SPI device")
+      print("Could not")
 
     # silly cleanup
     #gpio_reset(  8 )
@@ -35,18 +25,11 @@ class HC595():
     #gpio_reset( 16 )
     #gpio_reset(  5 )
 
-    GPIO.setup(self.cs, GPIO.OUT)
-    GPIO.output(self.cs, GPIO.LOW)
+    #GPIO.setup( self.gpio_pin, GPIO.IN )
+    #GPIO.setup(self.gpio_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN )
+    GPIO.setup(self.gpio_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP )
 
-
-  def write( self, val ):
-    self.spiRW( val, self.speed, 20 )  
-    GPIO.output(self.cs, GPIO.HIGH)
-    time.sleep( 0.000001 )
-    GPIO.output(self.cs, GPIO.LOW)
-
-  def spiRW(self, values, speed, delay):
-    #print("values are: ", values )
-    #msg = self.spi.xfer( values, speed, delay )
-    self.spi.writebytes( values )
+  def read_it( self ):
+    val = GPIO.input( self.gpio_pin )
+    return val
 
--- adapters/sensors/Panasonic_PM-25_photosensor/test_PM25_photosensor.py
+++ adapters/sensors/Panasonic_PM-25_photosensor/test_PM25_photosensor.py
@@ -2,52 +2,15 @@
 
 import time
 
-import HC595_shift_reg as shifter
+import PM25_photosensor as PM_25
 
-reg = shifter.HC595()
+photosensor = PM_25.PM_25()
 
-
-
-seq = [ [ 1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 0, 0,  1, 0, 1, 0 ],
-        [ 0, 0, 1, 0,  0, 0, 1, 0,  0, 0, 1, 0,  0, 0, 1, 0 ],
-        [ 0, 0, 0, 0,  1, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0 ],
-        [ 0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  1, 0, 0, 0 ],
-        [ 0, 1, 0, 1,  0, 1, 0, 1,  0, 1, 0, 1,  0, 0, 1, 1 ] ]
-        
-#seq = [ [ 1, 0, 0, 0,  0, 0, 0, 0  ],
-#        [ 0, 1, 0, 0,  0, 0, 0, 0  ],
-#        [ 0, 0, 1, 0,  0, 0, 0, 0  ],
-#        [ 0, 0, 0, 1,  0, 0, 0, 0  ],
-#        [ 0, 0, 0, 0,  1, 0, 0, 0  ] ]
-
-#seq = [ 1, 0, 0, 0, 1, 0, 0, 0,  1, 0, 1, 0, 1, 0, 1, 0,  1, 0, 0, 1, 0, 0, 1, 0,   1, 0, 0, 1, 0, 0, 1, 1 ]
-seq_step = 0
-
-val = [ 0 ]
 while True:
 
-    val[ 0 ] = 0;
-    
-    for trk in range( 0, 5 ):
-    
-        if seq[ trk ][ seq_step ] == 1:    
-            val[ 0 ] = val[ 0 ] + ( 1 << trk )
-            #val[ 0 ] = val[ 0 ] | 0xff
-            
+    val = photosensor.read_it()
     print( val )
-    reg.write( val )
-    time.sleep( .010 )
-
-    val[ 0 ] = 0x00
-    print( val )
-    reg.write( val )
-    time.sleep( 0.205 )
-
-    seq_step = seq_step + 1
-    if seq_step >= 16:
-        seq_step = 0
-    
-    
+    time.sleep( 0.0205 )
 
 
 
