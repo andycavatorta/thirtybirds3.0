d4d2e84c4ab36279646c040b24ccaf14db9da283
Noah Vawter <diydsp@yahoo.com>
2021-04-08 17:21:43 +0000

    added new test for all 25 solenoids in a long sequence

---
+++ adapters/actuators/chime_solenoid/super_direct_GPIO.py
@@ -0,0 +1,35 @@
+import time
+import RPi.GPIO as GPIO
+
+
+class gpio_based():
+  def __init__(self, chimeGPIOs, deviceId=0 ):
+    self.deviceId = deviceId
+    self.chimeGPIOs = chimeGPIOs
+
+    GPIO.setmode(GPIO.BCM)
+
+    # config and turn off
+    for gpio in self.chimeGPIOs:
+      GPIO.setup( gpio, GPIO.OUT )
+      GPIO.output( gpio, GPIO.LOW )
+
+
+  # send a vector of 5 ints, no bit packing etc. to GPIOs
+  def write( self, chime_vec ):
+    #print("values are: ", chime_vec )
+    for idx in range( 0, len( chime_vec ) ):
+      outval = GPIO.LOW
+      chime_val = chime_vec[ idx ]
+      if chime_val != 0:
+        outval = GPIO.HIGH
+      gpio = self.chimeGPIOs[ idx ]
+      GPIO.output( gpio, outval )
+    
+  # disable GPIOs driving
+  def disable_GPIOs( self ):
+    for gpio in self.chimeGPIOs:
+      GPIO.output( gpio, GPIO.LOW )
+      GPIO.setup( gpio, GPIO.IN )
+    
+
