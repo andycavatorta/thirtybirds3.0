8420a78b273cefef0f3d8ef873e1ecf872c79102
Matt Ross <mattross1080@gmail.com>
2021-04-30 17:18:22 -0400

    print message to clinet

--- network/dashboard.py
+++ network/dashboard.py
@@ -30,18 +30,19 @@ class SimpleChat(WebSocket):
        #       client.sendMessage(self.address[0] + u' - ' + self.data)
 
     def handleConnected(self):
-       print(self.address, 'connected')
+    #    print(self.address, 'connected')
        for client in clients:
           client.sendMessage(self.address[0] + u' - connected')
        clients.append(self)
 
     def handleClose(self):
        clients.remove(self)
-       print(self.address, 'closed')
+    #    print(self.address, 'closed')
        for client in clients:
           client.sendMessage(self.address[0] + u' - disconnected')
 
     def sendToClients(self, message):
+       print("Sending message to clinet : ", message)
        for client in clients:
           client.sendMessage(message)
 
