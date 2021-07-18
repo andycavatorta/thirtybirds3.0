5e185a151325933d3f2707c44d49fa3d10beddb8
Andy Cavatorta <hellyeah@media.mit.edu>
2021-07-17 19:32:56 -0500

    adding debug printout to encoder read to debug

--- adapters/sensors/AMT203_encoder/AMT203_absolute_encoder.py
+++ adapters/sensors/AMT203_encoder/AMT203_absolute_encoder.py
@@ -35,12 +35,12 @@ class AMT203():
   def get_position(self):
     #GPIO.output(self.cs, GPIO.LOW)
     attempts = 0
-    #print(">>> 1")
+    print(">>> 1")
 
     # send rd_pos, expect nop_a5 return value
     first_result = self.spiRW([0x10],self.speed,20)  
-    #print( 'first res: ', first_result[ 0 ], ', ', end='' )
-    #print(">>> 2")
+    print( 'first res: ', first_result[ 0 ], ', ', end='' )
+    print(">>> 2")
 
     # keep sending NOP/0x00 as long as response is nop_a5
     while True:
@@ -53,30 +53,30 @@ class AMT203():
       if first_result[ 0 ] != 0x10:
         #print( 'got: ', first_result[0], ', ', end ='' )
         break;
-    #print(">>> 4")
+    print(">>> 4")
 
     attempts = 0
     while True:
       tmp = self.spiRW( [0x00], self.speed, 20 )
-      #print( tmp, ',, ', end='' )
+      print( tmp, ',, ', end='' )
       if tmp[ 0 ] == 0x10:
         msb_result = self.spiRW( [0x00], self.speed, 20 )
         break;
       attempts = attempts + 1
       if attempts > 100: 
-        #print(" yuk2 ")
+        print(" yuk2 ")
         #return -1
         pass
 
-    #print(">>> 5")
+    print(">>> 5")
     lsb_result = self.spiRW([0x00],self.speed,20)
-    #print( hex( msb_result[ 0 ] ), ', ', end ='' )
-    #print( hex( lsb_result[ 0 ] ), ', ',  end = '' )
-    #print(">>> 6")
+    print( hex( msb_result[ 0 ] ), ', ', end ='' )
+    print( hex( lsb_result[ 0 ] ), ', ',  end = '' )
+    print(">>> 6")
     final_result = (msb_result[0]<<8 | lsb_result[0])
-    #print(">>> 7")
+    print(">>> 7")
     self.clean_buffer()
-    #print(">>> 8")
+    print(">>> 8")
 
     #GPIO.output(self.cs, GPIO.HIGH )
     return final_result
@@ -95,7 +95,8 @@ class AMT203():
 
   def spiRW(self, values, speed, delay):
     GPIO.output(self.cs, GPIO.LOW)
-    time.sleep(.01)
+    #time.sleep(.01)    GPIO.output(self.cs, GPIO.LOW)
+
     msg = self.spi.xfer(values, speed, delay)
     GPIO.output(self.cs, GPIO.HIGH)
     return msg
