376711a566d4353c2bfbe923cb80b4041e7b7c6d
Andy Cavatorta <hellyeah@media.mit.edu>
2021-10-09 20:38:46 -0500

    incrementally testing mixed use of encoders

--- adapters/sensors/ina260/ina260.py
+++ adapters/sensors/ina260/ina260.py
@@ -3,30 +3,51 @@ import board
 import adafruit_ina260
 
 class INA260():
-    def __init__(self):
+    def __init__(self,callback):
+        self.callback = callback
         try:
             i2c = board.I2C()
             self.ina260 = adafruit_ina260.INA260(i2c)
+            self.present = True
         except ValueError:
-            print("INA260 not found")
-            #add feedback through tb?
-    def get_current(self):
-        return self.ina260.current
+            self.present = False
 
-    def get_min_max_sample(self, ms=1000):
-        end_time = time.time() + (ms/1000.0)
-        minimum_current = 0
-        maximum_current = 0
-        while time.time() < end_time:
-            current = self.ina260.current
-            if minimum_current > current:
-                minimum_current = current
-            if maximum_current < current:
-                maximum_current = current
-        return [minimum_current, maximum_current]
+    def get_present(self):
+        return self.present
 
     def get_voltage(self):
-        return self.ina260.voltage
+        try:
+            return self.ina260.voltage
+        except Exception:
+            self.callback("present",False)
+            return None
 
     def get_power(self):
-        return self.ina260.power
+        try:
+            return self.ina260.power
+        except Exception:
+            self.callback("present",False)
+            return None
+
+    def get_current(self):
+        try:
+            return self.ina260.current
+        except Exception:
+            self.callback("present",False)
+            return None
+
+    def get_min_max_sample(self, ms=1000):
+        try:
+            end_time = time.time() + (ms/1000.0)
+            minimum_current = 0
+            maximum_current = 0
+            while time.time() < end_time:
+                current = self.ina260.current
+                if minimum_current > current:
+                    minimum_current = current
+                if maximum_current < current:
+                    maximum_current = current
+            return [minimum_current, maximum_current]
+        except Exception:
+            self.callback("present",False)
+            return None
