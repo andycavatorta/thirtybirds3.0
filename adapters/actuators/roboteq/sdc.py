780eed8b267cea40173075577e87c9519aea6304
Andy Cavatorta <hellyeah@media.mit.edu>
2022-07-26 19:28:19 -0400

    basic connections

--- adapters/actuators/roboteq/sdc.py
+++ adapters/actuators/roboteq/sdc.py
@@ -2445,14 +2445,11 @@ class SDC(threading.Thread):
             self.status_receiver("motor_controller_unresponsive")
             return False, command_response_l
         # the response should be in ["+","-"]
-        if len(command_response_l)>1: #if this is the wrong phase of the request
-            self.status_receiver("expected command ack/nak but got {}".format(command_response_l))
-            return False, command_response_l
-        if command_response_l[0]=="-":
-            self.status_receiver("motor controller returns nak")
-            return False, command_response_l
-        if command_response_l[0]=="+":
-            return True, command_response_l
+        if len(command_response_l)==2: #if this is the wrong phase of the request
+            return True, command_response_l[1]
+        else:
+            self.status_receiver("unexpected command response {}".format(command_response_l))
+            return False, ""
 
     def run(self):
         while True:
