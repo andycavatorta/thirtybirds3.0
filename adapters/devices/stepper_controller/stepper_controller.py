e1d25f505e79632e5dbff54d71c30e21879b0b1c
Andy Cavatorta <coffee@example.com>
2025-01-27 14:30:19 -0500

    first run

--- adapters/devices/stepper_controller/stepper_controller.py
+++ adapters/devices/stepper_controller/stepper_controller.py
@@ -223,10 +223,6 @@ class Controller(threading.Thread):
         self.pulses_per_revolution = pulses_per_revolution
         self.positive_is_clockwise = positive_is_clockwise
 
-
-        print("++++++++++++",direction_pin,pulse_pin,enable_pin)
-
-
         # gpios
         self.direction_output = output.Output(
             status_receiver, exception_receiver, direction_pin
