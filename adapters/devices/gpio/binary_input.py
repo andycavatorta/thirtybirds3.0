e80b904f68c72ad5ba3be020c068c138144dda47
Andy Cavatorta <coffee@example.com>
2025-03-04 17:41:17 -0500

    nudge

--- adapters/devices/gpio/binary_input.py
+++ adapters/devices/gpio/binary_input.py
@@ -196,4 +196,5 @@ class Inputs(threading.Thread):
             changes = self.get_changes()
             print(changes)
             for name, value in changes.items():
+                print(name, value)
                 self.event_receiver(name, event_names.CHANGE, value)
